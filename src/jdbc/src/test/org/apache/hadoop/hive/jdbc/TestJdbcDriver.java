b8f8cd38eebb3d7c78e426894a3e063b16f236d6
Curtis Boyden <cboyden@ewerp.com>
2011-06-01 12:13:41 -0400

    [PDI-5550] Fixing test cases

--- src/jdbc/src/test/org/apache/hadoop/hive/jdbc/TestJdbcDriver.java
+++ src/jdbc/src/test/org/apache/hadoop/hive/jdbc/TestJdbcDriver.java
@@ -51,9 +51,11 @@ public class TestJdbcDriver extends TestCase {
   private static String partitionedTableComment = "Partitioned table";
   private static String dataTypeTableName = "testDataTypeTable";
   private static String dataTypeTableComment = "Table with many column data types";
+  private static String preparedStatementTestTable = "testPreparedStmtTable";
   private final HiveConf conf;
   private final Path dataFilePath;
   private final Path dataTypeDataFilePath;
+  private final Path dataFilePathForPreparedStatementTests;
   private Connection con;
   private boolean standAloneServer = false;
 
@@ -64,6 +66,7 @@ public class TestJdbcDriver extends TestCase {
         .replace("c:", "");
     dataFilePath = new Path(dataFileDir, "kv1.txt");
     dataTypeDataFilePath = new Path(dataFileDir, "datatypes.txt");
+	dataFilePathForPreparedStatementTests = new Path(dataFileDir, "prepared_stmt.txt");
     standAloneServer = "true".equals(System
         .getProperty("test.service.standalone.server"));
   }
